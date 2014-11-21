5bd8f1694d0091ba648ed28949af67ee2adc13a4
Joshua Hoiland <hoilandj@eos14.cis.gvsu.edu>
2014-11-21 12:33:43 -0500

    Adds Meal table

--- create.sql
+++ create.sql
@@ -13,10 +13,27 @@ CREATE TABLE Ingredient
 	shelfLife INTEGER NOT NULL,
 	qty INTEGER NOT NULL,
 	
+	--Primary key
 	CONSTRAINT iIC1 PRIMARY KEY(iName, iSize),
+	--isVeg must be T or F
 	CONSTRAINT iIC2 CHECK (isVeg in ('T','F'),
+	--isGF must be T or F
 	CONSTRAINT iIC2 CHECK (isGF in ('T','F')	
 );
 --Meal ( mID, name, timeOfDay, ldo, price ) 
-
+CREATE TABLE Meal
+(
+	meID INTEGER NOT NULL,
+	meName CHAR NOT NULL,
+	timeOfDay CHAR,
+	ldo DATE,
+	price FLOAT NOT NULL,
+	
+	--Primary key
+	CONSTRAINT meIC1 PRIMARY KEY(meID),
+	--timeOfDay must be breakfast, lunch, or dinner
+	CONSTRAINT meIC2 CHECK (timeOfDay in ('breakfast', 'lunch','dinner'),
+	--price must be greater than sum of ingredients
+	--??
+);
 

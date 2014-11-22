df02754eb15f36a78c4c29a40f0ae3d7bfd4a51a
Josh Hoiland <hoilandj3103@gmail.com>
2014-11-21 21:56:01 -0500

    adds some rows

--- create.sql
+++ create.sql
@@ -37,5 +37,12 @@ CREATE TABLE Meal
 	--??
 );
 INSERT INTO Ingredient VALUES ('sirloin', 9, 5.50, 200, 'F', 'T', 2, 40);
+INSERT INTO Ingredient VALUES ('sirloin', 7, 4.50, 175, 'F', 'T', 2, 40);
 INSERT INTO Ingredient VALUES ('mashed potato', 4, .92, 250, 'T', 'T', 3, 90);
+INSERT INTO Ingredient VALUES ('chicken breast', 4, 3.22, 250, 'F', 'T', 2, 40);
+INSERT INTO Ingredient VALUES ('romaine', 3, 1.15, 15, 'T', 'T', 2, 15);
+INSERT INTO Ingredient VALUES ('caesar', 2, .45, 200, 'F', 'T', 7, 200);
+INSERT INTO Ingredient VALUES ('chicken breast', 8, 4.22, 250, 'F', 'T', 2, 40);
 INSERT INTO Meal VALUES (0, 'House Steak', 'dinner', TO_DATE('11/19/14','MM/DD/YY'), 12.50);
+INSERT INTO Meal VALUES (1, 'Chicken Caesar Salad', 'dinner', TO_DATE('11/19/14','MM/DD/YY'), 9.00);
+INSERT INTO Meal VALUES (2, 'Chicken Caesar Salad', 'lunch', TO_DATE('11/19/14','MM/DD/YY'), 7.00);
